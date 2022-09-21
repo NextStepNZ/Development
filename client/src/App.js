ceacb5c2363d8a479b919737cec54d41c565ef66
Corban <corban@hackermail.com>
2022-09-22 04:17:08 +1200

    Added the AddUser component for testing.
    Completely over did the current layout of the application and made the Adduser testing page into its own component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,28 @@
-import "./App.css";
-import { useState } from "react";
-import axios from "axios";
+import React from 'react';
+import AddUser from "./components/AddUser";
+import User from './components/User';
+import "./App.css"
 
-function App() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+const App = () => {
 
-  /*
-  const displayInfo = () => {
-    console.log(username + " " + password);
-  };
-  */
 
-  const addNewUser = () => {
-    axios.post('http://localhost:3001/addUser', {
-      username: username, 
-      password: password
-    }).then(() => {
-      console.log("success");
-    })
-  }
+
+
+
 
   return (
-    <div className="App">
-      <h1>Next Step Landing Page</h1>
+    <>
+    <div className='App'>
+      <centre>
+        <h1>Landing Page</h1>
+        <br/>
+        <AddUser />
 
-      <div className="loginComponents">
-        <label>Username</label>
-        <input
-          type="Text"
-          placeholder="enter username here"
-          onChange={(event) => {
-            setUsername(event.target.value);
-          }}
-        />
-        <label>Password</label>
-        <input
-          type="Text"
-          placeholder="enter password here"
-          onChange={(event) => {
-            setPassword(event.target.value);
-          }}
-        />
-        <button onClick={addNewUser}>Add User and Password</button>
-      </div>
+      </centre>
+        <User firstname={'John'} lastname={'Smith'} isTeacher={'true'} />
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
