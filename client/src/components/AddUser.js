9a71673691557a1afc0b706b6fa947546864e260
SirAlphaa <captainscar117@gmail.com>
2022-09-22 05:40:10 +1200

    Cleaned Up Merge

--- client/src/components/AddUser.js
+++ client/src/components/AddUser.js
@@ -1,137 +1,86 @@
+import "./AddUser.css";
 import React from "react";
 import { useState } from "react";
+import { useRef } from "react";
 import axios from "axios";
 
 
 function AddUser() {
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-  
-    /*
-    const displayInfo = () => {
-      console.log(username + " " + password);
-    };
-    */
-  
-    const AddNewUser = () => {
-      axios.post('http://localhost:3001/addUser', {
-        username: username, 
-        password: password
-      }).then(() => {
-        console.log("success");
-      })
-    }
+  const [userlist, setUserlist] = useState([]);
 
-  return (
-    <>
-    <div className="AddUser">
-    <div className="loginComponents">
-      <label>Username</label>
-      <input
-        type="Text"
-        placeholder="enter username here"
-        onChange={(event) => {
-          setUsername(event.target.value);
-        }}
-      />
-      <label>Password</label>
-      <input
-        type="Text"
-        placeholder="enter password here"
-        onChange={(event) => {
-          setPassword(event.target.value);
-        }}
-      />
-      <button onClick={AddNewUser}>Add User and Password</button>
-    </div>
-  </div>
-  </>
-);
-  
-}
+  /*
+  const displayInfo = () => {
+    console.log(username + " " + password);
+  };
+  */
 
-export default AddUser
+  const userInput = useRef(null);
+  const passwordInput = useRef(null);
 
+  const addNewUser = event => {
+    axios.post('http://localhost:3001/addUser', { username: username, password: password })
+      .then(() => { console.log("success") });
 
+    userInput.current.value = '';
+    passwordInput.current.value = '';
+  }
 
+  const getUserList = () => {
+    axios.get('http://localhost:3001/getUserList').then((response) => { 
+      console.log(response) 
+      setUserlist(response.data)
+    });
+  }
 
-
-
-const [userlist, setUserlist] = useState([]);
-
-/*
-const displayInfo = () => {
-  console.log(username + " " + password);
-};
-*/
-
-const userInput = useRef(null);
-const passwordInput = useRef(null);
-
-const addNewUser = event => {
-  axios.post('http://localhost:3001/addUser', { username: username, password: password })
-    .then(() => { console.log("success") });
-
-  userInput.current.value = '';
-  passwordInput.current.value = '';
-}
-
-const getUserList = () => {
-  axios.get('http://localhost:3001/getUserList').then((response) => { 
-    console.log(response) 
-    setUserlist(response.data)
-  });
-}
-
-const deleteAllUsers = () => {
-  axios.post('http://localhost:3001/deleteAllUsers').then((response) => {
-    console.log(response)
-    getUserList()
-  })
-}
-
+  const deleteAllUsers = () => {
+    axios.post('http://localhost:3001/deleteAllUsers').then((response) => {
+      console.log(response)
+      getUserList()
+    })
+  }
 
 /*********************************************************************************************/
 
-return (
-  <div className="App">
-    <h1>DB Readwrite testpage</h1>
+  return (
+    <div className="App">
+      <h1>DB Readwrite testpage</h1>
 
-    <div className="loginComponents">
-      <label>Username</label>
-      <input
-        ref={userInput}
-        type="Text"
-        placeholder="enter username here"
-        onChange={(event) => {
-          setUsername(event.target.value);
-        }}
-      />
-      <label>Password</label>
-      <input
-        ref={passwordInput}
-        type="Text"
-        placeholder="enter password here"
-        onChange={(event) => {
-          setPassword(event.target.value);
-        }}
-      />
-      <button onClick={addNewUser}>Add User and Password</button>
-    </div>
-
-    ================================================================
-    <div className="displayComponents">
-      <div>
-      <button onClick={getUserList}> Show all Data </button>
-      <button onClick={deleteAllUsers}> Delete all data</button>
+      <div className="loginComponents">
+        <label>Username</label>
+        <input
+          ref={userInput}
+          type="Text"
+          placeholder="enter username here"
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <label>Password</label>
+        <input
+          ref={passwordInput}
+          type="Text"
+          placeholder="enter password here"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+        <button onClick={addNewUser}>Add User and Password</button>
       </div>
-      {userlist.map((val, key) => {
-        return <div className="displayComponentsList">
-          <h3>Username:</h3> <p>{val.userName}</p>
-          <h3>Password:</h3> <p>{val.password}</p>
-          </div>
-      })}
 
+      ================================================================
+      <div className="displayComponents">
+        <div>
+        <button onClick={getUserList}> Show all Data </button>
+        <button onClick={deleteAllUsers}> Delete all data</button>
+        </div>
+        {userlist.map((val, key) => {
+          return <div className="displayComponentsList">
+            <h3>Username:</h3> <p>{val.userName}</p>
+            <h3>Password:</h3> <p>{val.password}</p>
+            </div>
+        })}
+
+      </div>
     </div>
-  </div>)
+)}
