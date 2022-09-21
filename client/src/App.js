283a00674f46b2d86e140a8892ea7b863835b002
SirAlphaa <captainscar117@gmail.com>
2022-09-22 05:44:21 +1200

    Cleaned Up Merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import React from "react";
-import { useState } from "react";
-import { useRef } from "react";
-import axios from "axios";
 
-return (
-    <div className='App'>
-        <h1>Landing Page</h1>
-        <br/>
-        <AddUser />
-        <User firstname={'John'} lastname={'Smith'} isTeacher={'true'} />
-    </div>
-  )
+function App() {
+  return (
+      <div className='App'>
+          <h1>Landing Page</h1>
+          <br/>
+          <AddUser />
+          <User firstname={'John'} lastname={'Smith'} isTeacher={'true'} />
+      </div>
+    )
+  
+}
 
 
 export default App;
