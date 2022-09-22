1835a38fdd508b783b2a95f68a9c5d1a3fd96993
Corban <corban@hackermail.com>
2022-09-22 21:37:18 +1200

    Started working on login and authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import AddUser from "./components/AddUser";
 import User from './components/User';
 import Test from './components/Test';
-import {Routes, Route} from 'react-router-dom';
+import Login from './components/Login/Login';
+
+//import Dashboard from './components/Dashboard/Dashboard';
+//import Preferences from './components/Preferences/Preferences';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 const App = () => {
-  
+  const [token, setToken] = useState();  
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
+
   return (
     <>
+    <div className="wrap">
     <Navbar />
-    <Routes> 
-      <Route path='/' element={<Home />}/>
-      <Route path='/adduser' element={<AddUser />}/>
-      <Route path='/user' element={<User firstname={'John'} lastname={'Smith'} isTeacher={'true'}/>}/>
-      <Route path='/Test' element={<Test/>}/>
-    </Routes>
+      <BrowserRouter>
+        <Routes> 
+          <Route path='/' element={<Home />}/>
+          <Route path='/adduser' element={<AddUser />}/>
+          <Route path='/user' element={<User firstname={'John'} lastname={'Smith'} isTeacher={'true'}/>}/>
+          <Route path='/Test' element={<Test/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
     </>
     );
 }
