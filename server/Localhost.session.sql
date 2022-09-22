1775bdb9677cc15be75841cb29cfa1d932d0e7e2
SirAlphaa <captainscar117@gmail.com>
2022-09-22 19:34:28 +1200

    Test

--- server/Localhost.session.sql
+++ server/Localhost.session.sql
@@ -4,3 +4,16 @@ SELECT * FROM logindetails
 
 -- @block
 DELETE FROM logindetails
+
+-- @block 
+
+CREATE SCHEMA `nextstepnz` ;
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+
+CREATE TABLE `nextstepnz`.`logindetails` (
+  `idloginDetails` INT NOT NULL AUTO_INCREMENT,
+  `userName` TEXT(255) NOT NULL,
+  `password` TEXT(255) NOT NULL,
+  PRIMARY KEY (`idloginDetails`));
+  
