639da1b316562a1d10abd84d24a6f6ba3d4b3cd5
xmp4998 <83616948+xmp4998@users.noreply.github.com>
2022-09-22 14:13:15 +1200

    Test.js add
    Have a small test can be run

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import AddUser from "./components/AddUser";
 import User from './components/User';
+import Test from './components/Test';
 import {Routes, Route} from 'react-router-dom';
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <Route path='/' element={<Home />}/>
       <Route path='/adduser' element={<AddUser />}/>
       <Route path='/user' element={<User firstname={'John'} lastname={'Smith'} isTeacher={'true'}/>}/>
+      <Route path='/Test' element={<Test/>}/>
     </Routes>
     </>
     );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,23 +3,26 @@ import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
-return(
-    <>
-     <nav className="nav">
-          <h1 className="nav-title">Next Step</h1>
-         <ul>
-              <li>
-                  <Link to="/">Home</Link>
-              </li>
-              <li>
-                  <Link to="/adduser">Add User</Link>
-              </li>
-              <li>
-                  <Link to="/user">User</Link>
-              </li>
-            </ul>
-        </nav>
-    </>
-);
+    return (
+        <>
+            <nav className="nav">
+                <h1 className="nav-title">Next Step</h1>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/adduser">Add User</Link>
+                    </li>
+                    <li>
+                        <Link to="/user">User</Link>
+                    </li>
+                    <li>
+                        <Link to="/Test">Test</Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
 }
 export default Navbar;
