b92a44b2e9c2977be6bc9931f73e2c68d4b97edf
mvr4024 <83377965+mvr4024@users.noreply.github.com>
2022-09-23 17:48:40 +1200

    Adding about information

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,23 @@
-/*import React from 'react'
-import { Link } from "react-router-dom";*/
+import React from 'react'
 
 function Home() {
+    return(
+        <body>
+        <h1>About NextStep</h1>
+        <p1>NextStep is a resource built to help both students
+            and teachers directly when it comes to learning and testing.
+            NextStep is aimed at creating the perfect quiz platform to help
+            those who want to learn. <n/>
+            NextStep will allow teachers to assign quizzes as well as check the 
+            results of the students in their respective classes. <n/>
+            Students who are proud of what they've learnt and their scores
+            that they have achieved will be able to purchase cosmetic items
+            for their avatars with points earned from completing quizes
+        </p1>
 
+    </body>
+
+    );
 }
 
 export default Home
