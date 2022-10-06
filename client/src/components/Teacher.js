f518f4427f1f6865d11584fe30cab3a7b35a11b1
SirAlphaa <captainscar117@gmail.com>
2022-10-06 16:52:57 +1300

    Added new tab/component Teacher.JS - This will be used for all the teach controls eg. Adding quizzes and viewing results

---
+++ client/src/components/Teacher.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Teacher() {
+    return(
+        
+        <div>
+            Hello World, this is the teacher panel!
+        </div>
+        
+    );
+}
+
+export default Teacher
