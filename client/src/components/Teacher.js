bb9ff4ee4d94759fc272a0076655969c5c5bbc20
SirAlphaa <captainscar117@gmail.com>
2022-10-08 16:54:30 +1300

    Added components to Teacher.js - Working on Student search

--- client/src/components/Teacher.js
+++ client/src/components/Teacher.js
@@ -1,12 +1,40 @@
 import React from "react";
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { TextField, Grid, Button} from "@mui/material";
 
 function Teacher() {
-    return(
-        
-        <div>
-            Hello World, Look into MUI for this page 
-        </div>
-        
+    return (
+        <>
+            <div className="App">
+
+                <Typography variant="h2" gutterBottom>
+                    Teacher Panel
+                </Typography>
+
+                <Typography variant="h5" gutterBottom>
+                    Student Search
+                </Typography>
+
+                <Box
+                    component="form"
+                    sx={10}
+                    noValidate
+                    autoComplete="off"
+                >
+
+                            <TextField id="studentIDInput" label="ID Number" variant="outlined" />
+
+                            <TextField id="studentFNInput" label="First Name" variant="outlined" />
+
+                            <TextField id="studentLNInput" label="Last Name" variant="outlined" />
+
+                            <Button variant="contained">Search</Button>
+
+                </Box>
+
+            </div>
+        </>
     );
 }
 
