6886735ef583334642f84aed13817b6a5207ed65
SirAlphaa <captainscar117@gmail.com>
2022-10-12 22:48:07 +1300

    Added Teacher Search Functionality - Users are now able to search for all userprofiles using components - User can chose to both or either fname or lname to make searches

--- client/src/components/Teacher.js
+++ client/src/components/Teacher.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { TextField, Grid, Button} from "@mui/material";
+import { TextField, Button } from "@mui/material";
+import { useState } from "react";
+import axios from "axios";
 
 function Teacher() {
+
+    const [studentFName, setStudentFName] = useState("");
+    const [studentLName, setStudentLName] = useState("");
+    const [studentDetails, setStudentDetails] = useState([]);
+    const studentFNInput = useRef(null);
+    const studentLNInput = useRef(null);
+    const searchStudent = () => {
+        console.log("🚀 ~ file: Teacher.js ~ line 14 ~ Teacher ~ studentFName", studentFName)
+        console.log("🚀 ~ file: Teacher.js ~ line 15 ~ Teacher ~ studentLName", studentLName)
+        axios.post('http://localhost:3001/searchStudent', { studentFName: studentFName, studentLName: studentLName })
+            .then((Response) => {
+                console.log("🚀 ~ file: Teacher.js ~ line 11 ~ searchStudent ~ Response", Response)
+                setStudentDetails(Response.data)
+            }
+        )
+        studentFNInput.current.value = '';
+        studentLNInput.current.value = '';
+    }
+
     return (
         <>
             <div className="App">
@@ -18,18 +39,28 @@ function Teacher() {
 
                 <Box
                     component="form"
-                    sx={10}
+                    sm={12}
                     noValidate
                     autoComplete="off"
                 >
 
-                            <TextField id="studentIDInput" label="ID Number" variant="outlined" />
-
-                            <TextField id="studentFNInput" label="First Name" variant="outlined" />
-
-                            <TextField id="studentLNInput" label="Last Name" variant="outlined" />
-
-                            <Button variant="contained">Search</Button>
+                    <TextField
+                        inputRef={studentFNInput}
+                        label="First Name"
+                        variant="outlined"
+                        onChange={(event) => {
+                            setStudentFName(event.target.value);
+                        }}
+                    />
+                    <TextField
+                        inputRef={studentLNInput}
+                        label="Last Name"
+                        variant="outlined"
+                        onChange={(event) => {
+                            setStudentLName(event.target.value);
+                        }}
+                    />
+                    <Button variant="contained" onClick={searchStudent}>Search</Button>
 
                 </Box>
 
