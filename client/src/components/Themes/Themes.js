f024435d5b396fb50da239d408164b3c135f2c31
mvr4024 <83377965+mvr4024@users.noreply.github.com>
2022-10-25 15:45:35 +1300

    Updating themes

--- client/src/components/Themes/Themes.js
+++ client/src/components/Themes/Themes.js
@@ -1,12 +1,12 @@
 export const lightTheme = {
-    body: '#FFF',
+    body: '#DEF2F1',
     text: '#363537',
     toggleBorder: '#FFF',
     background: '#363537',
 }
 export const darkTheme = {
     body: '#363537',
-    text: '#FAFAFA',
+    text: '#DEF2F1',
     color: '#FFFFFF',
     toggleBorder: '#6B8096',
     background: '#999',
